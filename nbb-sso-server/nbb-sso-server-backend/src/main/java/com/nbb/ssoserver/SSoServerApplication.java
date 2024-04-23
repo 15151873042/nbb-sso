3d3728f9c93586a0d83abd3a8856967ceb8fdcfe
hp <15151873042@163.com>
2024-04-23 17:59:08 +0800

    添加启动参数日志

--- nbb-sso-server/nbb-sso-server-backend/src/main/java/com/nbb/ssoserver/SSoServerApplication.java
+++ nbb-sso-server/nbb-sso-server-backend/src/main/java/com/nbb/ssoserver/SSoServerApplication.java
@@ -1,13 +1,17 @@
 package com.nbb.ssoserver;
 
 
+import cn.dev33.satoken.SaManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SSoServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SSoServerApplication.class, args);
+        log.info("Sa-Token配置如下：{}", SaManager.getConfig());
     }
 }
